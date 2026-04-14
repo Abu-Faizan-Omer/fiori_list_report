05411cb4685b1b8476089b06c27f3f6a87018490
Abu Faizan Omer <abufaizan1997@gmail.com>
2026-04-14 09:26:20 +0000

    V2 add anotation

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.listreport.zf68felrsegw.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
